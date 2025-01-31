dc061fcadc7cf9ac6c6bd15d5b45f441f55bccb5
Siriphan.Phakmi <b_siriphan@hotmail.com>
2025-02-01 00:32:49 +0700

    Deploy ขึ้น Firebase https://manpower-patient-summary.web.app/

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  distDir: 'out',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
