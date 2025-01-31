480c0fd3ab2def805be8e7069ab5d20922414567
Siriphan.Phakmi <b_siriphan@hotmail.com>
2025-02-01 00:52:37 +0700

    แก้ไขการ deploy

--- app/page.js
+++ app/page.js
@@ -10,26 +10,33 @@ export default function Home() {
     <div>
       {/* Navigation */}
       <nav className="bg-[#0ab4ab] p-4">
-        <div className="container mx-auto">
-          <div className="flex justify-center space-x-4">
-            <button
-              onClick={() => setCurrentPage('form')}
-              className={`px-4 py-2 rounded-lg ${currentPage === 'form'
-                ? 'bg-white text-[#0ab4ab]'
-                : 'text-white hover:bg-[#0ab4ab]/80'
-                }`}
-            >
-              บันทึกข้อมูล
-            </button>
-            <button
-              onClick={() => setCurrentPage('dashboard')}
-              className={`px-4 py-2 rounded-lg ${currentPage === 'dashboard'
-                ? 'bg-white text-[#0ab4ab]'
-                : 'text-white hover:bg-[#0ab4ab]/80'
-                }`}
-            >
-              Dashboard
-            </button>
+        <div className="container mx-auto relative">
+          {/* ปุ่มอยู่ตรงกลาง */}
+          <div className="flex justify-center">
+            <div className="flex space-x-4">
+              <button
+                onClick={() => setCurrentPage('form')}
+                className={`px-4 py-2 rounded-lg ${currentPage === 'form'
+                  ? 'bg-white text-[#0ab4ab]'
+                  : 'text-white hover:bg-[#0ab4ab]/80'
+                  }`}
+              >
+                บันทึกข้อมูล
+              </button>
+              <button
+                onClick={() => setCurrentPage('dashboard')}
+                className={`px-4 py-2 rounded-lg ${currentPage === 'dashboard'
+                  ? 'bg-white text-[#0ab4ab]'
+                  : 'text-white hover:bg-[#0ab4ab]/80'
+                  }`}
+              >
+                Dashboard
+              </button>
+            </div>
+          </div>
+          {/* เวอร์ชันอยู่ขวาสุด */}
+          <div className="absolute right-0 top-1/2 transform -translate-y-1/2 text-white text-sm">
+            v1.1.2.2025 beta
           </div>
         </div>
       </nav>
